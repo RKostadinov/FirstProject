aaf9ab247cf27f8c27338c2c45fa656f66de5036
RKostadinov <radoslav97@abv.bg>
2014-04-10 23:50:38 +0300

    Adding first 3 files from the task

---
+++ card.h
@@ -0,0 +1,14 @@
+#ifndef __CARD__
+#define __GAME__
+
+struct card_t
+    {
+        char name[50];
+        int damage;
+        int hp;
+        int mana;
+    };
+
+int attack(struct card_t *attacker, struct card_t *defender);
+
+#endif
